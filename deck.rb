1622e66e14c7718973eb28626f3eb3839a27f134
ChuDik <chudik@inbox.ru>
2020-04-14 09:51:33 +0500

    create Deck class with array of cards and shuffle

--- deck.rb
+++ deck.rb
@@ -1,5 +1,20 @@
 class Deck
   def initialize
+    @deck = create_deck
+  end
 
+  def create_deck
+    suites = ["+", "^", "<", "<>"]
+    ranks_digital = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+    ranks_high = ['J', 'Q', 'K', 'A']
+    suites.each do |suit|
+      ranks_digital.each do |rank_digital|
+        @deck << Card.new(suit, rank_digital)
+      end
+      ranks_high.each do |rank_high|
+        @deck << Card.new(suit, rank_high)
+      end
+    end
+    @deck.shuffle.reverse.shuffle
   end
 end
