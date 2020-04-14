988397928f814a8feeb68357934f135082b712fe
ChuDik <chudik@inbox.ru>
2020-04-14 14:23:11 +0500

    some styling with rubocop

--- deck.rb
+++ deck.rb
@@ -3,18 +3,17 @@ require_relative './card'
 class Deck
   attr_reader :cards
 
-
   def initialize
     @cards = create_deck
   end
 
   def create_deck
     @cards = []
-    ['+', '^', '<', '<>'].each do |suit|
+    %w[+ ^ < <>].each do |suit|
       (2..10).each do |face|
         @cards << Card.new(suit, face)
       end
-      ['J', 'Q', 'K', 'A'].each do |face|
+      %w[J Q K A].each do |face|
         @cards << Card.new(suit, face)
       end
     end
