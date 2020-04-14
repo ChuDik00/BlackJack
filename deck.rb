fc53c4a79f9749c0f967e070f8f8d37b5290f37e
ChuDik <chudik@inbox.ru>
2020-04-14 10:05:45 +0500

    logical change @deck to @cards. Stupid deck.deck - deck.cards is better

--- deck.rb
+++ deck.rb
@@ -1,29 +1,29 @@
 require_relative 'card'
 
 class Deck
-  attr_reader :deck
+  attr_reader :cards
 
   def initialize
-    @deck = create_deck
+    @cards = create_deck
   end
 
   def create_deck
-    @deck = []
-    suites = ["+", "^", "<", "<>"]
+    @cards = []
+    suits = ["+", "^", "<", "<>"]
     ranks_digital = [2, 3, 4, 5, 6, 7, 8, 9, 10]
     ranks_high = ['J', 'Q', 'K', 'A']
-    suites.each do |suit|
+    suits.each do |suit|
       ranks_digital.each do |rank_digital|
-        @deck << Card.new(suit, rank_digital)
+        @cards << Card.new(suit, rank_digital)
       end
       ranks_high.each do |rank_high|
-        @deck << Card.new(suit, rank_high)
+        @cards << Card.new(suit, rank_high)
       end
     end
-    @deck.shuffle.reverse.shuffle
+    @cards.shuffle.reverse.shuffle
   end
 end
 
 deck = Deck.new
-p deck.deck
-p deck.deck.size
+p deck.cards
+p deck.cards.size
