2c1a2a2993c807e2c5701af07bbbe4d5439ebddd
ChuDik <chudik@inbox.ru>
2020-04-14 14:01:47 +0500

    add count_points method for count points on hands

--- hand.rb
+++ hand.rb
@@ -12,4 +12,13 @@ class Hand
       puts "#{count+1}: #{card.face}#{card.suit}"
     end
   end
+
+  def count_points
+    points = 0
+    @cards.each do |card|
+      points += card.rank
+    end
+  end
+
+
 end
