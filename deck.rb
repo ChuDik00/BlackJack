9af5c3a8730b51abde306ef94f31fc33aeb85240
ChuDik <chudik@inbox.ru>
2020-04-14 11:09:03 +0500

    remove validation from deck, optimise length of create_deck method

--- deck.rb
+++ deck.rb
@@ -1,24 +1,20 @@
-require_relative 'card'
+require_relative './card'
 
 class Deck
-  include Validation
   attr_reader :cards
 
-  validate :cards, :dulicate
+
   def initialize
     @cards = create_deck
   end
 
   def create_deck
     @cards = []
-    suits = ["+", "^", "<", "<>"]
-    digital_faces = [2, 3, 4, 5, 6, 7, 8, 9, 10]
-    higher_faces = ['J', 'Q', 'K', 'A']
-    suits.each do |suit|
-      digital_faces.each do |face|
+    ['+', '^', '<', '<>'].each do |suit|
+      (2..10).each do |face|
         @cards << Card.new(suit, face)
       end
-      higher_faces.each do |face|
+      ['J', 'Q', 'K', 'A'].each do |face|
         @cards << Card.new(suit, face)
       end
     end
@@ -27,5 +23,4 @@ class Deck
 end
 
 deck = Deck.new
-p deck.cards
-p deck.cards.size
+p deck
