e51c0c2920b11ee429b8d63496a2e8e79719cfba
ChuDik <chudik@inbox.ru>
2020-04-14 13:40:24 +0500

    add show_cards method to see what cards on hands of players

--- hand.rb
+++ hand.rb
@@ -2,4 +2,14 @@ class Hand
   def initialize
     @cards = []
   end
+
+  def deal!(deck)
+    @cards << deck.cards.shift
+  end
+
+  def show_cards
+    @cards.each_with_index do |card, count|
+      puts "#{count+1}: #{card.face}#{card.suit}"
+    end
+  end
 end
