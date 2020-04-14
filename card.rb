91bd0cd9ad0818d96b353a1d188ba723997b7fc3
ChuDik <chudik@inbox.ru>
2020-04-14 11:18:13 +0500

    add rank of cards

--- card.rb
+++ card.rb
@@ -1,8 +1,14 @@
 class Card
-  attr_reader :suit, :face
+  attr_reader :suit, :face, :rank
 
   def initialize(suit, face)
     @suit = suit
     @face = face
+    @rank = rank
+  end
+
+  def rank
+    return 10 if ['J', 'Q', 'K'].include?(@face)
+    @rank
   end
 end
