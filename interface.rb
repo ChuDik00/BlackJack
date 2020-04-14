53279c752b2efe9eb7a53e6b23a11ab34c2c3058
ChuDik <chudik@inbox.ru>
2020-04-14 14:34:03 +0500

    add Hand class to interface for cheking

--- interface.rb
+++ interface.rb
@@ -3,30 +3,34 @@ require_relative './deck'
 require_relative './player'
 require_relative './user'
 require_relative './dealer'
+require_relative './hand'
 require_relative './validation'
 
 def create_user
   print 'Insert your name: '
   name = gets.chomp
-  user = User.new(name)
-  p user
+  User.new(name)
 rescue RuntimeError => e
   puts e.message
   retry
 end
 
 def create_dealer
-  dealer = Dealer.new
-  p dealer
+  Dealer.new
 rescue RuntimeError => e
   puts e.message
   retry
 end
 
 def build_deck
-  deck = Deck.new
-  p deck
+  Deck.new
 end
-create_user
-create_dealer
-build_deck
+
+user = create_user
+dealer = create_dealer
+deck = build_deck
+user.hand = Hand.new
+dealer.hand = Hand.new
+p user
+p dealer
+p deck
