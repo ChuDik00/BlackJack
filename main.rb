0c77b45d9be7e7b287eb40fc71c2f8e1bce3bfcc
ChuDik <chudik@inbox.ru>
2020-04-16 19:50:59 +0500

    some refactoring

--- main.rb
+++ main.rb
@@ -40,9 +40,9 @@ loop do
   # game.user_hand
   # game.dealer_hand
 
-  return 'Dead heat!' if status.zero?
-  return 'Your victory!' if status.positive?
-  return 'Dealer victory!' if status.negative?
+  puts 'Dead heat!' if status.zero?
+  puts 'Your victory!' if status.positive?
+  puts 'Dealer victory!' if status.negative?
 
 
   #Выводится баланс игрока и баланс дилера
